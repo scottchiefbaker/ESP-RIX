fda6a4fe2b8f6f71f8beda5cd712cf02cfbc261b
Scott Baker <scott@perturb.org>
2023-02-20 10:09:23 -0800

    Simplify example

--- examples/01-levels.ino
+++ examples/01-levels.ino
@@ -9,23 +9,15 @@ void setup() {
 void loop() {
 	handle_rix();
 
-	rix_1("This is an ALERT");
-	rix_2("This is CRITICAL %0.1f", 8.1);
-	rix_3("This is an error %d", 37);
-	rix_4("This is a warning %s", "BUB!");
-	rix_5("This is a %s", "NOTICE");
-	rix_6("INFORMATION: Uptime: %d", millis());
-	rix_7("This is just debug %s", "Scott");
-
-	test_func();
-
-	rix_5("");
+	rix_1("ALERT message");
+	rix_2("CRITICAL message");
+	rix_3("ERROR message");
+	rix_4("WARNING message");
+	rix_5("NOTICE message");
+	rix_6("INFORMATION message");
+	rix_7("DEBUG message");
 
 	Serial.println("Loooping");
 
 	rix_delay(2000);
-}
-
-void test_func () {
-	rix_7("Testing functions %s", __func__);
 }
